efa76cf595f2d85c0d8b01322b8128a1553c02d7
Jofera <jofera@gmail.com>
2023-01-24 19:47:04 +0100

    Create recipe model

--- src/app/recipes/recipe-detail/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-detail/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test', 'https://i.blogs.es/8968d6/risotto-a-los-cuatro-quesos/840_560.jpg')
+  ];
 
+  constructor(){
+     
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
